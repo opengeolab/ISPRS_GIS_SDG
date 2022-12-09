db21583236b8181f87e5a2cc27718307658d3a8d
Julia Leonardi <juliaanna.leonardi@mail.polimi.it>
2022-12-10 00:02:17 +0100

    4.2 added only figures

--- conf.py
+++ conf.py
@@ -59,7 +59,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_logo = './img/IMG_2364.jpg'
+html_logo = './img/LOGO.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
